d84ed08f3c3da09082ac4bcc2578b5952d0f4ca9
payton <plinteq14@icloud.com>
2025-09-30 23:33:30 -0400

    everything

--- source.js
+++ source.js
@@ -2,3 +2,21 @@ function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav-container").classList.toggle("change");
 }
+
+function filterImages(filterName) {
+    if(filterName != "ALL") { 
+        var x = document.getElementsByClassName(filterName);
+
+    for(var i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+
+    var y = document.getElementById(filterName).style.display = "block";
+    }
+    else {
+        var x = document.getElementsByClassName(filterName);
+        for(var i = 0; i < x.length; i++){
+            x[i].style.display = "block";
+            }
+    }
+}
